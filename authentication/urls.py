9b63871039579d2559cbd17801789b0533ccc63e
Umut Topalak <umuttopalak@hotmail.com>
2024-04-10 01:05:36 +0300

    url changed

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from .api import UserSignUpAPI
 
 urlpatterns = [
-    path('signup/', UserSignUpAPI.as_view()),
+    path('register/', UserSignUpAPI.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('revoke-token/', TokenRefreshView.as_view()),
 ]
