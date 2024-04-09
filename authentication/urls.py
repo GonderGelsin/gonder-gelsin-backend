c95dadca0df5622f0dc5e0029abfbe3c602fee44
Umut Topalak <umuttopalak@hotmail.com>
2024-04-10 02:51:09 +0300

    some fix

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+                                            TokenRefreshView,
+                                            )
 
 from .api import UserSignUpAPI
 
