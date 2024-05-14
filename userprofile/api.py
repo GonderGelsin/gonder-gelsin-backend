8da64edb987b3c8ef3f074c6d30d4be3268ce23b
Umut Topalak <umuttopalak@hotmail.com>
2024-05-15 01:30:19 +0300

    users list endpoint created - will be deleted

--- userprofile/api.py
+++ userprofile/api.py
@@ -40,3 +40,11 @@ class UserProfileDetailAPI(APIView):
             return CustomSuccessResponse(status_code=status.HTTP_200_OK)
 
         return CustomErrorResponse(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+class TestAPI(APIView):
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = UserProfileSerializer(users, many=True)
+        return CustomSuccessResponse(input_data=serializer.data)
+    
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .api import UserProfileDetailAPI
 
 urlpatterns = [
-    path("", UserProfileDetailAPI.as_view())    
+    path("", UserProfileDetailAPI.as_view()),
+    path("users/", UserProfileDetailAPI.as_view()),
+
 ]
