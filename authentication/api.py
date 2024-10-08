97b17586c1452d94120e77424547b33b9f0806fb
Umut Topalak <umuttopalak@hotmail.com>
2024-10-08 22:12:28 +0300

    log added

--- authentication/api.py
+++ authentication/api.py
@@ -208,6 +208,7 @@ def google_sign_in(request):
         return CustomErrorResponse(status_code=status.HTTP_400_BAD_REQUEST)
         
     except Exception as e:
+        logger.warning(str(e))
         return CustomErrorResponse(msj=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     # except ValueError as e:
     #     return Response({'error': f"Invalid token : {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
