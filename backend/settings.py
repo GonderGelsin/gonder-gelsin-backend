7489fb4217d96035461780e1e34a97b34e0a9ae3
Umut Topalak <umuttopalak@hotmail.com>
2024-12-21 12:05:22 +0300

    Refactor Firebase credentials configuration to use environment variables
    - Replaced hardcoded Firebase credentials in backend/settings.py with a dictionary that retrieves values from environment variables.
    - This change enhances security by avoiding the use of sensitive information in the codebase.

--- backend/settings.py
+++ backend/settings.py
@@ -194,8 +194,22 @@ LOGGING = {
 }
 
 
-cred = credentials.Certificate('./credentials.json')
+# Replace with environment variable based configuration
+firebase_credentials = {
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+}
 
+cred = credentials.Certificate(firebase_credentials)
 FIREBASE_APP = initialize_app(credential=cred)
 
 FCM_DJANGO_SETTINGS = {
