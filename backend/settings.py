a3cea525ee0e87d44c874ae39ddf1d6fdf92b198
Umut Topalak <umuttopalak@hotmail.com>
2025-02-16 14:42:54 +0300

    Refactor Firebase credentials handling to load from a JSON string in environment variable

--- backend/settings.py
+++ backend/settings.py
@@ -195,21 +195,23 @@ LOGGING = {
 
 
 # Replace with environment variable based configuration
-firebase_credentials = {
-    "type": os.environ.get("FIREBASE_TYPE"),
-    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
-    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
-}
+# firebase_credentials = {
+#     "type": os.environ.get("FIREBASE_TYPE"),
+#     "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+#     "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+#     "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+#     "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+#     "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+#     "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+#     "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+#     "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+#     "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+#     "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
+# }
 
-cred = credentials.Certificate(firebase_credentials)
+firebase_credentials = os.environ.get("FIREBASE_CREDENTIALS")
+firebase_credentials_dict = json.loads(firebase_credentials)
+cred = credentials.Certificate(firebase_credentials_dict)
 FIREBASE_APP = initialize_app(credential=cred)
 
 FCM_DJANGO_SETTINGS = {
