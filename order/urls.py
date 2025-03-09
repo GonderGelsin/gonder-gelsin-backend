f6df50c62e6dc81e1325dd0bc43260b475e0211b
Umut Topalak <umuttopalak@hotmail.com>
2025-03-09 18:22:02 +0300

    feat(order): add OrderDetailAPIView for retrieving individual orders

--- order/urls.py
+++ order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .api import (ActiveOrdersAPIView, ComplatedOrdersAPIView,
-                  DeleteOrderAPIView, OrderListCreateAPIView,
-                  UpdateOrderStatusAPIView)
+                  DeleteOrderAPIView, OrderDetailAPIView,
+                  OrderListCreateAPIView, UpdateOrderStatusAPIView)
 
 urlpatterns = [
     path('', OrderListCreateAPIView.as_view()),
+    path('<int:pk>/', OrderDetailAPIView.as_view()),
     path('active/', ActiveOrdersAPIView.as_view()),
     path('complated/', ComplatedOrdersAPIView.as_view()),
     path('delete/<int:pk>/', DeleteOrderAPIView.as_view()),
