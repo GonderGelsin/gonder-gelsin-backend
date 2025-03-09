99056731a8f92613e2ba0d0c73e1292a26cbfad3
Umut Topalak <umuttopalak@hotmail.com>
2025-03-10 01:29:01 +0300

    feat(notification): refactor notification URLs and add user-specific routes

--- notification/urls.py
+++ notification/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from .api import (NotificationDetail, NotificationListCreate, NotificationRead,
-                  SendNotificationAPI)
+from .api import SendGeneralNotificationAPI
 
 urlpatterns = [
-    path('', NotificationListCreate.as_view(), name='notification-list-create'),
-    path('<int:pk>/', NotificationDetail.as_view(), name='notification-detail'),
-    path('read/', NotificationRead.as_view(), name='notification-read'),
-    path('send/', SendNotificationAPI.as_view(), name='notification-send'),
+    path('general/send/', SendGeneralNotificationAPI.as_view(), name='general-notification-send'),
 
 ]
